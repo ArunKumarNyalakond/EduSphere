0255810aa25b11798e970b21da642529f028ed21
Arun Kumar Nyalakonda <arunkumarnyalakond@lewisu.edu>
2025-11-02 01:57:13 +0530

    first commit

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useState, useEffect } from "react";
+import { dummyCourses } from "../assets/assets";
+
+export const AppContext = createContext();
+
+export const AppContextProvider = (props) => {
+  const currency = import.meta.env.VITE_CURRENCY || "$";
+  const [allCourses, setAllCourses] = useState([]);
+
+  const fetchAllCourses = async () => {
+    // Just load dummy data for now
+    setAllCourses(dummyCourses);
+  };
+
+  useEffect(() => {
+    fetchAllCourses();
+  }, []);
+
+  const value = {
+    currency,
+    allCourses,
+    fetchAllCourses,
+  };
+
+  return (
+    <AppContext.Provider value={value}>
+      {props.children}
+    </AppContext.Provider>
+  );
+};
